2e955cfdada6dde463c31ab1245bbc86f42a1a39
JP Strydom <jp.strydom@entelect.co.za>
2017-12-15 14:27:40 +0200

    Change loading structure

--- src/components/loading-screen/loading-screen.view.js
+++ src/components/loading-screen/loading-screen.view.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Spinner from 'react-native-loading-spinner-overlay';
 
 export default class LoadingScreen extends Component {
@@ -6,3 +7,7 @@ export default class LoadingScreen extends Component {
         return <Spinner visible={this.props.showLoadingScreen} overlayColor="rgba(0,22,78,0.7)" />;
     }
 }
+
+LoadingScreen.propTypes = {
+    showLoadingScreen: PropTypes.bool.isRequired
+};
---
+++ src/components/loading-spinner/loading-spinner.view.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ActivityIndicator } from 'react-native';
+
+export default class LoadingSpinner extends Component {
+    render() {
+        return <ActivityIndicator color="#00164e" animating={this.props.showLoadingSpinner} />;
+    }
+}
+
+LoadingSpinner.propTypes = {
+    showLoadingSpinner: PropTypes.bool.isRequired
+};
